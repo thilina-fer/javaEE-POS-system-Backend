f7557c90d97ef54e784157aa6ca681aee7d1d83b
thilina-fer <dilshanthilina1010@gmail.com>
2025-12-24 07:37:20 +0530

    feat: implement get all customers API endpoint

--- src/main/java/lk/ijse/CustomerServlet.java
+++ src/main/java/lk/ijse/CustomerServlet.java
@@ -1,6 +1,7 @@
 package lk.ijse;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
@@ -48,5 +49,36 @@ public class CustomerServlet extends HttpServlet {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    //======================= Get All Customers
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Connection connection = basicDataSource.getConnection();
+            String sql = "SELECT * FROM customers";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            JsonArray result = new JsonArray();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String address = resultSet.getString("address");
+                String contact = resultSet.getString("contact");
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", id);
+                jsonObject.addProperty("name", name);
+                jsonObject.addProperty("address", address);
+                jsonObject.addProperty("contact", contact);
+                result.add(jsonObject);
+            }
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().println(result);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
